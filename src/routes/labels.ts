b43fdd32292bd056cfcbf1eee007a74532f1acc7
hannahellis4242 <hannahellis4242@gmail.com>
2024-03-07 22:07:06 +0000

    fixing labels page some more, including sorting out the fact that links don't show up well against the background

--- src/routes/labels.ts
+++ src/routes/labels.ts
@@ -1,30 +1,42 @@
-import axios from "axios";
-import { Router } from "express";
+import axios, { Axios, AxiosError } from "axios";
+import { Router, response } from "express";
 import Config from "../Config/Config";
 
 interface Dictionary {
   [key: string]: string[];
 }
 
+const recover = (error: unknown) => {
+  if (!(error instanceof AxiosError)) {
+    return Promise.reject(error);
+  }
+  const { response } = error;
+  if (!response) {
+    return Promise.reject(error);
+  }
+  const { status } = response;
+  if (!status) {
+    return Promise.reject(error);
+  }
+  if (status !== 404) {
+    return Promise.reject(error);
+  }
+  return Promise.resolve([]);
+};
+
 const labels = ({ bookmarks }: Config) =>
   Router().get("/", async (_, res) => {
     try {
-      console.log("labels");
       const labels = await axios
         .get<string[]>(`http://${bookmarks.host}:${bookmarks.port}/label/all`)
         .then(({ data }) => data)
-        .catch((e) => {
-          console.log(`*** ERROR ***\n${e}\n=============`);
-          return Promise.resolve([]);
-        });
-      console.log(`Got labels : ${JSON.stringify(labels)}`);
+        .catch(recover);
       const grouped = labels.reduce<Dictionary>((acc, x) => {
         const k = x.charAt(0);
         const cur = acc[k];
         cur.push(x);
         return acc;
       }, {});
-      console.log(`groups : ${JSON.stringify(grouped)}`);
       res.render("./labels", { grouped, isEmpty: labels.length === 0 });
     } catch (e) {
       console.error(e);
